b02e8890f5abcb1e79090cc8fb4882808eae0c2a
YourNerdiness <91400199+YourNerdiness@users.noreply.github.com>
2022-01-21 14:54:07 +1100

    Added createWindow method.

--- GameEngine/DisplayManager.java
+++ GameEngine/DisplayManager.java
@@ -4,6 +4,41 @@ import javax.swing.*;
 
 public class DisplayManager extends GameEngine {
 
-    
+    JFrame frame;
+    JPanel panel;
+
+    public void createWindow(int x, int y, int frameCloseOperation) {
+
+        frame = new JFrame();
+        panel = new JPanel(null);
+
+        panel.setSize(x, y);
+        frame.setSize(x, y);
+
+        pixels = new JButton[x][y];
+
+        for (int i = 0; i < x; i++) {
+
+            for (int j = 0; j < y; j++) {
+
+                JButton pixel = pixels[i][j];
+
+                pixel = new JButton();
+                pixel.setBounds(x, y, 1, 1);
+                panel.add(pixel);
+
+
+            }
+
+        }
+
+        frame.add(panel);
+
+        frame.setDefaultCloseOperation(frameCloseOperation);
+
+        panel.setVisible(true);
+        frame.setVisible(true);
+
+    }
 
 }
--- GameEngine/GameEngine.java
+++ GameEngine/GameEngine.java
@@ -6,14 +6,12 @@ public class GameEngine {
 
     //Variables accesed by multiple classes
 
-    JButton[] pixels;
+    JButton[][] pixels;
 
     public static void main(String[] args) {
 
         DisplayManager displayManager = new DisplayManager();
         InputManager inputManager = new InputManager();
-
-
 
     }
 
